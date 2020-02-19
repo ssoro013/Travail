07f5c5b901e9ce59860a35c701cb4cc715e458bf
Soro Didiouf <ssoro013@gmail.com>
2020-02-18 19:34:25 -0800

    JobList Component

--- client/components/JobList.jsx
+++ client/components/JobList.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Job from './Job.jsx';
 
 var JobList = (props) => {
+
     return (
         <div>
             {props.jobs.map(job => {
                 return (
                     <div
                         key = {job.id}>
-                        <Job job={job} company={props.companies.filter(company => company.id === job.company_id)}></Job>
+                        <Job job={job} company={props.companies.filter(company => company.id === job.company_id)} showForm={props.showForm}></Job>
                     </div>
                 )
             })}
