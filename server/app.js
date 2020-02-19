c7e2859763c30deecf2c42cad1f999bd901f271c
Soro Didiouf <ssoro013@gmail.com>
2020-02-18 19:34:46 -0800

    New Routes

--- server/app.js
+++ server/app.js
@@ -5,16 +5,19 @@ var cors = require('cors');
 var compression = require('compression');
 var morgan = require ('morgan');
 var bodyParser = require('body-parser');
-var db = require('../database/db.js');
 var routes = require('./routes.js')
 
+app.use(express.static('public'));
 
+//middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(compression());
 app.use(morgan('tiny'));
-app.use(express.static('public'));
+
+//routes
+app.use('/', routes);
 
 module.exports = app;
 module.exports.port = port;
