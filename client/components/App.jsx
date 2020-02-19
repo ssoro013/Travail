2332e790d31bdb8df5b6e6cb58593bc574d46ab1
Soro Didiouf <ssoro013@gmail.com>
2020-02-18 19:35:52 -0800

    Front-End

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,24 +1,68 @@
 import React from 'react';
 import axios from 'axios';
 import JobList from './JobList.jsx';
-import Filter from './Filter.jsx';
+import Location from './Filters/Location.jsx';
 import Sort from './Sort.jsx';
+import Form from './Form.jsx';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            jobs: [],
+            companies: [],
+            locations: [],
+            salary: [],
+            funding: [],
+            round: [],
+            status: [],
+            show: false,
         }
+        this.setLocations = this.setLocations.bind(this);
+        this.showForm = this.showForm.bind(this);
+        this.hideForm = this.hideForm.bind(this);
     }
 
+    setLocations(locations) {
+        this.setState({
+            locations: locations
+        })
+    }
+
+    showForm() {
+        this.setState({
+            show: true
+        })
+    }
+    
+    hideForm() {
+        this.setState({
+            show: false
+        })
+    }
     componentDidMount() {
+        axios.all([
+            axios.get('/companies'),
+            axios.get('/jobs'),
+            axios.get('/locations')
+        ])
+        .then(axios.spread((companies, jobs, locations) => {
+            this.setState({
+                jobs: jobs.data,
+                companies: companies.data,
+                locations: locations.data.map(location => location.city)
+            })
+        }))
+        .then(() => console.log(this.state.locations))
+        .catch(error => console.log(error))
     }
 
     render() {
         return (
             <div>
-                Travail!
+                <p><Location setLocations={this.setLocations} locations={this.state.locations}></Location></p>
+                <div style={{display: (this.state.show ? 'block': 'none')}}><Form></Form></div>
+                {(this.state.companies.length && this.state.jobs.length) && <JobList jobs={this.state.jobs.filter((job) => this.state.locations.includes(job.city))} companies={this.state.companies} showForm={this.showForm}></JobList>}
             </div>
         )
     }
---
+++ client/components/Form.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import styled from 'styled-components';
+
+var FormDiv = styled.div `
+    display: inline-block;
+    text-align: left;
+    float: right;
+`
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            first: '',
+            last: '',
+            email: '',
+            phone: '',
+            location: '',
+            resume: '',
+            cover: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        var name = event.target.name;
+        var value = event.target.value;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit(event) {
+
+    }
+
+    render() {
+        var {first, last, email, phone, location, resume, cover} = this.state
+        return(
+            <FormDiv>
+
+            <form onSubmit = {this.handleSubmit}>
+                <p>
+                    <div><label>First Name</label></div>
+                    <input name = 'first' value = {first} onChange = {this.handleChange} size="50"></input>
+                </p>
+                <p>
+                    <div><label>Last Name</label></div>
+                    <input name = 'last' value = {last} onChange = {this.handleChange} size="50"></input>
+                </p>
+                <p>
+                    <div><label>Email</label></div>
+                    <input name = 'email' value = {email} onChange = {this.handleChange} size="50"></input>
+                </p>
+                <p>
+                    <div><label>Phone Number</label></div>
+                    <input name = 'phone' value = {phone} onChange = {this.handleChange} size="50"></input>
+                </p>
+                <p>
+                    <div><label>Location</label></div>
+                    <input name = 'location' value = {location} onChange = {this.handleChange} size="50"></input>
+                </p>
+                <p>
+                    <label>
+                        Resume/CV
+                        <input name = 'resume' type='file'></input>
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Cover Letter
+                        <input name = 'cover' type='file'></input>
+                    </label>
+                </p>
+               <button type='submit' value='Submit'>Apply</button>
+            </form>
+            </FormDiv>
+        )
+    }
+}
+
+export default Form;
