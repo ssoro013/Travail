4c5b1033c25bcdc77aa293730c929578d03a4eb2
Soro Didiouf <ssoro013@gmail.com>
2020-02-20 11:49:33 -0800

    Form Test

---
+++ test/form.test.js
@@ -0,0 +1,19 @@
+import { shallow, mount, render } from 'enzyme';
+import React from 'react';
+import Form from '../client/components/Form.jsx';
+
+describe('Testing the application form', () => {
+    it('Should expect the form to exist', () => {
+        var wrap = shallow(<Form />)
+        expect(wrap.exists()).toBe(true)
+    })
+
+    it('Should expect the form to have an apply button', () => {
+        var wrap = shallow(<Form />)
+        expect(wrap.find('button').at(0).text()).toEqual('Apply')
+    })
+    it('Should expect the form to have a cancel button', () => {
+        var wrap = shallow(<Form />)
+        expect(wrap.find('button').at(1).text()).toEqual('Cancel')
+    })
+})
