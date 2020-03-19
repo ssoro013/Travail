1cf831bcb05f33be3885a7abee4d24a6629f8022
Soro Didiouf <ssoro013@gmail.com>
2020-03-18 17:51:04 -0700

    Chart

--- client/components/Chart.jsx
+++ client/components/Chart.jsx
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
 import {
-  LineChart,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-  Legend,
-  Line,
-  ComposedChart,
-  Area,
-  Bar
+    AreaChart,
+    CartesianGrid,
+    XAxis,
+    YAxis,
+    Tooltip,
+    Area
 } from "recharts";
 
 
@@ -22,24 +18,24 @@ class Chart extends Component {
     render() {
         var data = this.props.employees;
         return (
-        <div className="App">
+        <div>
             <header>
                 <h1>Employees</h1>
             </header>
             <h2 style={{ textAlign: "left" }}></h2>
-            <LineChart
-                width={750}
-                height={250}
-                data={data}
-                margin={{ top: 5, right: 30, left: 5, bottom: 5 }}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
+            <AreaChart width={730} height={250} data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                <defs>
+                    <linearGradient id="employees" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+                        <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+                    </linearGradient>
+                </defs>
                 <XAxis dataKey="name" />
                 <YAxis />
+                <CartesianGrid strokeDasharray="3 3" />
                 <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="count" stroke="#8884d8" />
-            </LineChart>
+                <Area type="monotone" dataKey="count" stroke="#8884d8" fillOpacity={1} fill="url(#employees)" />
+            </AreaChart>
       </div>
       );
     }
