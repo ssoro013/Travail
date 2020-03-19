e6c2215934de84f94a8928d2f97eb0fc9b62aa1d
Soro Didiouf <ssoro013@gmail.com>
2020-03-18 18:43:27 -0700

    Refactor Chart into Functional Component

--- client/components/Chart.jsx
+++ client/components/Chart.jsx
@@ -9,35 +9,28 @@ import {
 } from "recharts";
 
 
-class Chart extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
-    }
-    render() {
-        var data = this.props.employees;
-        return (
-        <div>
-            <header>
-                <h1>Employees</h1>
-            </header>
-            <h2 style={{ textAlign: "left" }}></h2>
-            <AreaChart width={730} height={250} data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-                <defs>
-                    <linearGradient id="employees" x1="0" y1="0" x2="0" y2="1">
-                        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-                        <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
-                    </linearGradient>
-                </defs>
-                <XAxis dataKey="name" />
-                <YAxis />
-                <CartesianGrid strokeDasharray="3 3" />
-                <Tooltip />
-                <Area type="monotone" dataKey="count" stroke="#8884d8" fillOpacity={1} fill="url(#employees)" />
-            </AreaChart>
-      </div>
-      );
-    }
+var Chart = (props) => {
+    var data = props.employees;
+    return (
+    <div>
+        <header>
+            <h1>Employees</h1>
+        </header>
+        <AreaChart width={730} height={250} data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+            <defs>
+                <linearGradient id="employees" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+                    <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+                </linearGradient>
+            </defs>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <CartesianGrid strokeDasharray="5 5" />
+            <Tooltip />
+            <Area type="monotone" dataKey="count" stroke="#8884d8" fillOpacity={2} fill="url(#employees)" />
+        </AreaChart>
+    </div>
+    );
 }
+
 export default Chart;
