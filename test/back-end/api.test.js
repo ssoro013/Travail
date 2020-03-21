05145090ba640c0b1ac54f8089ac97ca2b2b43d0
Soro Didiouf <ssoro013@gmail.com>
2020-03-20 17:38:54 -0700

    Api Tests

--- test/back-end/api.test.js
+++ test/back-end/api.test.js
@@ -2,13 +2,59 @@ import "regenerator-runtime/runtime";
 import "core-js/stable";
 
 var request = require("supertest");
-
 var server = require('../../server/app');
 
 describe('GET /companies', () => {
-    test('It should responds with an array of companies of length 7', async() => {
-        var response = await request(server).get('/companies');
+    test('It should responds with an array of companies of length 7', async(done) => {
+        var response = await request(server)
+        .get('/companies')
         expect(response.body.length).toEqual(7);
         expect(response.statusCode).toBe(200);
+        done();
+    });
+});
+
+describe('Get /jobs', () => {
+    test('', async(done) => {
+        var response = await request(server)
+        .get('/jobs')
+        expect([200, 304]).toContain(response.statusCode);
+        done();
+    });
+});
+
+describe('Get /employees', () => {
+    test('', async(done) => {
+        var response = await request(server)
+        .get('/employees')
+        expect([200, 304]).toContain(response.statusCode);
+        done();
+    });
+});
+
+describe('Get /locations', () => {
+    test('', async(done) => {
+        var response = await request(server)
+        .get('/locations')
+        expect([200, 304]).toContain(response.statusCode);
+        done();
+    });
+});
+
+describe('Get /rounds', () => {
+    test('', async(done) => {
+        var response = await request(server)
+        .get('/rounds')
+        expect([200, 304]).toContain(response.statusCode);
+        done();
+    });
+});
+
+describe('Get /status', () => {
+    test('', async(done) => {
+        var response = await request(server)
+        .get('/status')
+        expect([200, 304]).toContain(response.statusCode);
+        done();
     });
 });
