c265281dd621eef9b512538460c58cb5123829dd
Soro Didiouf <ssoro013@gmail.com>
2020-03-23 15:52:56 -0700

    Added Route

--- client/components/JobList.jsx
+++ client/components/JobList.jsx
@@ -1,17 +1,36 @@
 import React, { useContext, Suspense } from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import styled from 'styled-components';
 var Job = React.lazy(()=> import('./Job.jsx'));
 import Loading from './Loading.jsx';
+
+var Pre = styled.div `
+    background-color: white;          
+    margin: 32px auto 0;
+    padding: 16px;
+    max-width: 750px;
+    width: 100%;
+    border: 1px solid lightgray;
+    font-family: Avenir, sans-serif;
+`
 
 var JobList = (props) => {
     return (
         <div>
             {props.jobs.map(job => {
                 return (
-                    <div key = {job.id}>
+                    <Pre key = {job.id}>
                         <Suspense fallback={<Loading />}>
-                            <Job job={job} showForm={props.showForm} updateJobId={props.updateJobId} employees={props.employees.filter(employee => employee.company === job.company)} />
+                            <Router>
+                                <Link to={`/${job.id}`}>{job.title}</Link>
+                                    <Switch>
+                                        <Route exact path={`/${job.id}`}>
+                                            <Job job={job} showForm={props.showForm} updateJobId={props.updateJobId} employees={props.employees.filter(employee => employee.company === job.company)} />
+                                        </Route>
+                                    </Switch>
+                            </Router>
                         </Suspense>
-                    </div>
+                    </Pre>
                 )
             })}
         </div>    
